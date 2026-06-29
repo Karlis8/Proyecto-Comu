0a33729a1c37491ff66a0ffc63764780d031b26f
Karla P. Méndez Sánchez <karla.mendezsanchez@ucr.ac.cr>
2026-06-29 16:05:50 -0600

    Pruebas de encendido de LED con GPIO.

---
+++ receptor.py
@@ -0,0 +1,22 @@
+
+#Amtes de correr
+#$ sudo apt update && sudo apt upgrade
+#$ sudo apt install python-rpi.gpio python3-rpi.gpio
+
+import RPi.GPIO as GPIO         # Import Raspberry Pi GPIO library
+from time import sleep          # Import the sleep function 
+
+pinLED = 12                      # LED GPIO Pin
+
+GPIO.setmode(GPIO.BCM)          # Use GPIO pin number
+GPIO.setwarnings(False)         # Ignore warnings in our case
+GPIO.setup(pinLED, GPIO.OUT)    # GPIO pin as output pin
+
+while True:                          # Endless Loop
+    GPIO.output(pinLED, GPIO.HIGH)   # Turn on
+  #  print("LED on")                    # Prints state to console
+    sleep(0.5)                         # Pause 0.5 second
+    GPIO.output(pinLED, GPIO.LOW)    # Turn off
+  #  print("LED off")                   # Prints state to console
+    sleep(0.5)                         # Pause 0.5 second
+
