62252d8b34ab8caecb0b7bed6ec5d4242766aecd
Karla P. Méndez Sánchez <karla.mendezsanchez@ucr.ac.cr>
2026-06-30 19:04:20 -0600

    Implement audio file transmission over NRF24
    Added functionality to read and transmit audio file in packets.

--- prueba_transmi.py
+++ prueba_transmi.py
@@ -1,33 +1,57 @@
 from RF24 import *
 import time
+import os
+import struct
 
 radio = RF24(22, 0)
 
-if not radio.begin():
-    print("No se pudo inicializar el NRF24")
-    quit()
-
-print("NRF24 detectado")
-
+radio.begin()
 radio.setChannel(76)
 radio.setDataRate(RF24_250KBPS)
 radio.setPALevel(RF24_PA_HIGH)
+radio.setAutoAck(True)
 
 direccion = b"TX_01"
 
 radio.openWritingPipe(direccion)
 radio.stopListening()
 
-radio.printPrettyDetails()
+ARCHIVO = "audio.wav"
 
-print("Comenzando transmisión...")
+tamano = os.path.getsize(ARCHIVO)
 
-while True:
+print("Tamaño:", tamano)
 
-    radio.write(
-        b"Hola"
-    )
+# Paquete START
+cabecera = b"START" + struct.pack("<I", tamano)
 
-    print("ENVIADO")
+radio.write(cabecera)
+time.sleep(0.1)
 
-    time.sleep(1)
+with open(ARCHIVO, "rb") as f:
+
+    numero = 0
+
+    while True:
+
+        datos = f.read(28)
+
+        if not datos:
+            break
+
+        paquete = struct.pack("<I", numero) + datos
+
+        ok = radio.write(paquete)
+
+        if ok:
+            print("Paquete", numero, "OK")
+        else:
+            print("Paquete", numero, "FALLO")
+
+        numero += 1
+
+        time.sleep(0.005)
+
+radio.write(b"END")
+
+print("Archivo enviado.")
